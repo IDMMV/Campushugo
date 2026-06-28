bb8b401c3cd1b400c70de2cef2feb36c520e28fb
Josehugo <josehugo.tec@gmail.com>
2026-06-28 18:06:05 -0400

    Delete assets directory

--- assets/config.js
+++
@@ -1,6 +0,0 @@
-// Reemplaza estos valores por los de tu proyecto en Supabase > Project Settings > API
-window.CAMPUSHUGO_CONFIG = {
-  SUPABASE_URL: 'https://TU-PROYECTO.supabase.co',
-  SUPABASE_ANON_KEY: 'TU_ANON_KEY',
-  WHATSAPP_NUMBER: '51999999999'
-};
