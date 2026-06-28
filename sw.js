2a3635372b208308c10ef4bcede190f7b0ba12db
Josehugo <josehugo.tec@gmail.com>
2026-06-28 18:29:33 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE='campushugo-oficial-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','css/styles.css','js/app.js','manifest.webmanifest']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
