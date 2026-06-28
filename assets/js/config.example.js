61b955d03ddd3b76a9b820e89d058cdc462cff61
Josehugo <josehugo.tec@gmail.com>
2026-06-28 16:43:40 -0400

    Delete assets/js directory

--- assets/js/config.example.js
+++
@@ -1,7 +0,0 @@
-// Copia este archivo como config.js cuando conectes Supabase.
-window.CH_CONFIG = {
-  SUPABASE_URL: 'https://TU-PROYECTO.supabase.co',
-  SUPABASE_ANON_KEY: 'TU_ANON_KEY',
-  WHATSAPP: '51999999999',
-  SITE_NAME: 'CampusHugo Pro'
-};
